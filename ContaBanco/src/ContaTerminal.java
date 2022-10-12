43a80bfe51f0f2246b0ce6aabba76a6ca49df0bb
CaiocsCunha <caiocscunha@gmail.com>
2022-10-12 19:17:42 -0300

    Projeto Conta Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o Nome!");
+        String nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o Saldo!");
+        double saldo = sc.nextDouble();
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque!\n");
     }
 }
